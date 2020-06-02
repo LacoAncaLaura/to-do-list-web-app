bf8af3d2e29304bd67f74fb6c86e2fc4438f9bb3
LacoAncaLaura <lala14444444@yahoo.com>
2020-06-02 21:32:54 +0300

    added and create task function

---
+++ js/tasks.js
@@ -0,0 +1,27 @@
+window.ToDoList = {
+    API_URL: 'http://localhost:8081/tasks',
+    createTasks:function () {
+        let descriptionValue = $('#description-field').val();
+        let deadlineValue = $('#deadline-field').val();
+
+        var requestBody = {
+            description: descriptionValue,
+            deadline: deadlineValue
+        };
+        $.ajax({
+            url: ToDoList.API_URL,
+            method:'POST',
+            contentType:'application/json',
+            data:JSON.stringify(requestBody)
+        }).done(function (response) {
+            console.log('success')
+            console.log(response);
+        });
+    },
+    bindEvents:function () {
+        $('#create-task-form').submit(function () {
+            ToDoList.createTasks();
+        })
+    }
+};
+ToDoList.bindEvents();
